3d0a91c638fc4ccada8effbb266c94fab945eeff
Veronica Trias <vero.ramm@gmail.com>
2022-10-22 09:24:38 -0300

    repositorio de portfolio

---
+++ Scripts/scripts.js
@@ -0,0 +1,47 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+      validadores[tipoDeInput](input);
+    }
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+      } else {
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML =
+         mostrarMensajeDeError(tipoDeInput, input);
+      }
+}
+    
+    const tipoDeErrores = [
+      "valueMissing",
+      "typeMismatch",
+     
+    ];
+  
+    const mensajesDeError = {
+      nombre: {
+        valueMissing: "El campo nombre no puede estar vacío",
+      },
+      email: {
+        valueMissing: "El campo correo no puede estar vacío",
+        typeMismatch: "El correo no es válido",
+      },
+      textarea:{
+        valueMissing: "Este campo no puede estar vacío",
+      },
+    }
+    const validadores = {
+      nacimiento: (input) => validarNacimiento(input),
+    };
+    function mostrarMensajeDeError(tipoDeInput, input){
+      let mensaje = "";
+      tipoDeErrores.forEach((error)=>{
+          if (input.validity[error]){
+              mensaje = mensajesDeError[tipoDeInput][error];
+          }
+      });
+      return mensaje;
+  }
+      
